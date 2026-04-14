33bbf79063feedb4524926fdcab453723fdbcb12
SushantYadav18 <shusant.kumar2bca@gmail.com>
2026-04-14 21:15:53 +0545

    new change

---
+++ AI.NotesGenerater/Frontend/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+
+
+  return (
+    <>
+      <div className="bg-blue-500 text-white p-4 rounded">
+        <h1 className="text-2xl font-bold text-center">AI Notes Generator</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
